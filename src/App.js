17f61348e8fb70dfa8379b65c3e3c2b161199319
Aziz Talbi <aziztalbi@azizs-air.localdomain>
2021-10-15 19:17:13 +0200

    work done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, useHistory,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/loggedIn">
+              <Users />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/loggedIn">Users</Link>
+          </li>
+        </ul>
+      </nav>
+  );
+}
+
+function Users() {
+
+    const history= useHistory();
+
+    const maxSeconds= 1800;
+
+    const [seconds, setSeconds] = React.useState(0)
+
+    React.useEffect(()=> {
+        if(seconds>maxSeconds)
+            history.push("/")
+        if(Date().toString().includes("00:00:00"))
+            history.push("/")
+    }, )
+
+    React.useEffect(() => {
+        setInterval(() => {
+            setSeconds(prevAmount => prevAmount + 1);
+        }, 1000);
+    }, [])
+
+    return (
+        <div>
+        <h2>You're session started</h2>
+        <h2>You'll be logged out in {maxSeconds} Seconds</h2>
+        <h3>Timer: {seconds}</h3>
+        </div>
+    );
+}
+
